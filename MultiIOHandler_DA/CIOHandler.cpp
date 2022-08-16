3a14e8115e2afc123db7377b593464ef9d3dfe39
AdrianHugentobler <adrian.hugentobler@hotmail.com>
2022-08-16 08:26:40 +0200

    added save/load commands

--- MultiIOHandler_DA/CIOHandler.cpp
+++ MultiIOHandler_DA/CIOHandler.cpp
@@ -126,12 +126,12 @@ void CIOHandler::callFunction(std::vector<std::string>* input)
 	else if (input->at(0) == "save")
 	{
 		CJSONHandler json;
-		json.save();
+		json.save(input->at(1));
 	}
 	else if (input->at(0) == "load")
 	{
 		CJSONHandler json;
-		json.load();
+		json.load(input->at(1));
 	}
 	else if (input->at(0) == "stop")
 	{
