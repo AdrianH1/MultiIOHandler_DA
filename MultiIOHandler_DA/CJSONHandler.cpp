6ff0c19e14465112e36202d18ca5b8b528adc8af
AdrianHugentobler <adrian.hugentobler@hotmail.com>
2022-08-16 10:22:28 +0200

    added save&load functionality

--- MultiIOHandler_DA/CJSONHandler.cpp
+++ MultiIOHandler_DA/CJSONHandler.cpp
@@ -1,45 +1,72 @@
 #include "CJSONHandler.h"
+#include "CSocketHandler.h"
+#include "CFileHandler.h"
 
 using json = nlohmann::json;
 
-void CJSONHandler::test()
+void CJSONHandler::save(std::string path, std::vector<IIOModule*> &modules)
 {
-	json jsonFile;
-	std::ofstream file("C:\\Users\\Adrian\\source\\repos\\AdrianH1\\key.json");
+	std::vector<std::string> info;
+	json json;
+	std::ofstream file(path);
+	for (int i = 0; i < modules.size(); i++)
+	{
+		if (modules.at(i)->m_type == 1) // Type 1 = Socket TCP
+		{
+			info = modules.at(i)->getInfo();
+			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"ip", info.at(2)}, {"port", stoi(info.at(3))}};
 
-	int id = 12;
-	std::string type = "socket";
-	std::string ip = "192.168.1.1";
-	int unsigned port = 65123;
-
-	jsonFile["Module1"] = { {"ID", id}, {"type", type}, {"ip", ip}, {"port", port} };
-
-	id = 13;
-	type = "socket";
-	ip = "193.168.1.1";
-	port = 65133;
-
-	jsonFile["Module2"]["ID"] = id;
-	jsonFile["Module2"]["type"] = type;
-	jsonFile["Module2"]["ip"] = ip;
-	jsonFile["Module2"]["port"] = port;
-
-	file << std::setw(4) << jsonFile << std::endl;
-
+		}
+		else if (modules.at(i)->m_type == 2) // Type 2 = Socket UDP
+		{
+			info = modules.at(i)->getInfo();
+			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"ip", info.at(2)}, {"port", stoi(info.at(3))} };
+		}
+		else if (modules.at(i)->m_type == 3) // Type 3 = File
+		{
+			info = modules.at(i)->getInfo();
+			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"path", info.at(2)} };
+		}
+		else if (modules.at(i)->m_type == 4) // Type 4 = Serial
+		{
+			//to do
+		}
+	}
+	file << std::setw(4) << json << std::endl;
 	file.close();
+}
 
-	std::cout << "writing done" << std::endl;
 
-	try
+void CJSONHandler::load(std::string path, std::vector<IIOModule*>& modules)
+{
+	std::cout << "Loading..." << std::endl;
+	std::fstream f(path);
+	json data = json::parse(f);
+	for (int i = 0; i < data.size(); i++)
 	{
-		std::fstream f("C:\\Users\\Adrian\\source\\repos\\AdrianH1\\key.json");
-		json data = json::parse(f);
-		std::cout << data;
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what();
-	}
+		if (data["Module" + std::to_string(i)]["type"] == 1) // Type 1 = Socket TCP
+		{
+			std::string ip = data["Module" + std::to_string(i)]["ip"].get<std::string>();
+			int port = data["Module" + std::to_string(i)]["port"].get<int>();
+			CSocketHandler* socket = new CSocketHandler(ip, port);
+			socket->init();
+			modules.push_back(socket);
+		}
+		else if (data["Module" + std::to_string(i)]["type"] == 2) // Type 2 = Socket UDP
+		{
 
-	std::cout << "reading done" << std::endl;
+		}
+		else if (data["Module" + std::to_string(i)]["type"] == 3) // Type 3 = File
+		{
+			std::string path = data["Module" + std::to_string(i)]["path"].get<std::string>();
+			CFileHandler* file = new CFileHandler(path);
+			file->init();
+			modules.push_back(file);
+		}
+		else if (data["Module" + std::to_string(i)]["type"] == 4) // Type 4 = Serial
+		{
+
+		}
+	}
+	std::cout << "Loading finished!" << std::endl;
 }
--- MultiIOHandler_DA/CJSONHandler.h
+++ MultiIOHandler_DA/CJSONHandler.h
@@ -1,11 +1,13 @@
 #include "stdafx.h"
+#include "IIOModule.h"
 #include <fstream>
 #include <nlohmann/json.hpp>
 
 class CJSONHandler
 {
 public:
-	void test();
+	void save(std::string path, std::vector<IIOModule*> &modules);
+	void load(std::string path, std::vector<IIOModule*>& modules);
 
 private:
 
