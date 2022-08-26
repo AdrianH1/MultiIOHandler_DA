62472e63b84987d067c1b9929191b2f870a9256d
Adrian Hugentobler <adrian.hugentobler@outlook.com>
2022-08-26 15:08:06 +0200

    changed m_type to enum

--- MultiIOHandler_DA/CJSONHandler.cpp
+++ MultiIOHandler_DA/CJSONHandler.cpp
@@ -15,7 +15,7 @@ void CJSONHandler::save(std::string path, std::vector<IIOModule*> &modules)
 		{
 		case IIOModule::type::clientSocket:
 			info = modules.at(i)->getInfo();
-			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"ip", info.at(2)}, {"port", stoi(info.at(3))} };
+			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"ip", info.at(2)}, {"port", stoi(info.at(3))}};
 			break;
 		case IIOModule::type::serverSocket:
 			info = modules.at(i)->getInfo();
@@ -42,11 +42,11 @@ void CJSONHandler::load(std::string path, std::vector<IIOModule*>& modules)
 	json data = json::parse(f);
 	for (int i = 0; i < data.size(); i++)
 	{
-		if (data["Module" + std::to_string(i)]["type"] == 0) // Type 0 = Server Socket
+		if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::serverSocket)
 		{
 
 		}
-		else if (data["Module" + std::to_string(i)]["type"] == 1) // Type 1 = Client Socket
+		else if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::clientSocket)
 		{
 			std::string ip = data["Module" + std::to_string(i)]["ip"].get<std::string>();
 			int port = data["Module" + std::to_string(i)]["port"].get<int>();
@@ -54,14 +54,14 @@ void CJSONHandler::load(std::string path, std::vector<IIOModule*>& modules)
 			socket->init();
 			modules.push_back(socket);
 		}
-		else if (data["Module" + std::to_string(i)]["type"] == 2) // Type 2 = File
+		else if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::file)
 		{
 			std::string path = data["Module" + std::to_string(i)]["path"].get<std::string>();
 			CFileHandler* file = new CFileHandler(path);
 			file->init();
 			modules.push_back(file);
 		}
-		else if (data["Module" + std::to_string(i)]["type"] == 3) // Type 3 = Serial
+		else if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::serial)
 		{
 
 		}
