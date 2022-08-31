64f18f14a9d911061bc4dbbe0ca2f2c9f16a0a29
Adrian Hugentobler <adrian.hugentobler@outlook.com>
2022-08-31 11:34:50 +0200

    added serversocket & serial functionality

--- MultiIOHandler_DA/CJSONHandler.cpp
+++ MultiIOHandler_DA/CJSONHandler.cpp
@@ -1,6 +1,8 @@
 #include "CJSONHandler.h"
 #include "CClientSocketHandler.h"
+#include "CServerSocketHandler.h"
 #include "CFileHandler.h"
+#include "CSerialHandler.h"
 
 using json = nlohmann::json;
 
@@ -19,13 +21,15 @@ void CJSONHandler::save(std::string path, std::vector<IIOModule*> &modules)
 			break;
 		case IIOModule::type::serverSocket:
 			info = modules.at(i)->getInfo();
-			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"ip", info.at(2)}, {"port", stoi(info.at(3))} };
+			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"port", stoi(info.at(2))} };
 			break;
 		case IIOModule::type::file:
 			info = modules.at(i)->getInfo();
 			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"path", info.at(2)} };
 			break;
 		case IIOModule::type::serial:
+			info = modules.at(i)->getInfo();
+			json["Module" + std::to_string(i)] = { {"id", stoi(info.at(0))}, {"type", stoi(info.at(1))}, {"port", info.at(2)}, {"baudrate", stoi(info.at(3))} };
 		default:
 			break;
 		}
@@ -44,7 +48,10 @@ void CJSONHandler::load(std::string path, std::vector<IIOModule*>& modules)
 	{
 		if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::serverSocket)
 		{
-
+			int port = data["Module" + std::to_string(i)]["port"].get<int>();
+			CServerSocketHandler* socket = new CServerSocketHandler(port);
+			socket->init();
+			modules.push_back(socket);
 		}
 		else if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::clientSocket)
 		{
@@ -63,7 +70,11 @@ void CJSONHandler::load(std::string path, std::vector<IIOModule*>& modules)
 		}
 		else if (data["Module" + std::to_string(i)]["type"] == IIOModule::type::serial)
 		{
-
+			std::string port = data["Module" + std::to_string(i)]["port"].get<std::string>();
+			int baudrate = data["Module" + std::to_string(i)]["baudrate"].get<int>();
+			CSerialHandler* serial = new CSerialHandler(port, baudrate);
+			serial->init();
+			modules.push_back(serial);
 		}
 	}
 	std::cout << "Loading finished!" << std::endl;
