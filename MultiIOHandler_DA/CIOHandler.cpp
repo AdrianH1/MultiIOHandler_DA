5552487f02e632a8c8dd6c4924f9350ae3d03ac4
Adrian Hugentobler <adrian.hugentobler@outlook.com>
2022-08-31 11:20:46 +0200

    changed function call handling

--- MultiIOHandler_DA/CIOHandler.cpp
+++ MultiIOHandler_DA/CIOHandler.cpp
@@ -11,6 +11,18 @@
 
 bool running = true;
 
+static const std::string sOpen = "open";
+static const std::string sConnect = "connect";
+static const std::string sInit = "init";
+static const std::string sOutput = "output";
+static const std::string sStop = "stop";
+static const std::string sRemove = "remove";
+static const std::string sShow = "show";
+static const std::string sSave = "save";
+static const std::string sLoad = "load";
+static const std::string sExit = "exit";
+
+
 void CIOHandler::createClientSocket(std::string ip, int unsigned port)
 {
 	CClientSocketHandler* socket = new CClientSocketHandler(ip, port);
@@ -39,15 +51,98 @@ void CIOHandler::createFile(std::string path)
 	modules.push_back(file);
 }
 
-void CIOHandler::connect(int id1, int id2)
+void CIOHandler::connectModules(int id1, int id2)
 {
-
+	IIOModule* first{};
+	for (IIOModule* m : modules)
+	{
+		if (m->getId() == id1)
+		{
+			first = m;
+		}
+	}
+	for (IIOModule* m : modules)
+	{
+		if (m->getId() == id2 && first != nullptr)
+		{
+			first->listenerTable.push_back(m);
+			first->connect();
+		}
+	}
 }
 
-void CIOHandler::disconnect()
+void CIOHandler::showModules()
 {
-
+	if (modules.size() == 0)
+	{
+		std::cout << "No modules available!";
+	}
+	else
+	{
+		for (IIOModule* m : modules)
+		{
+			m->printInfo();
+		}
+	}
 }
+
+void CIOHandler::initModules(int id)
+{
+	for (IIOModule* m : modules)
+	{
+		if (m->getId() == id)
+		{
+			m->init();
+			break;
+		}
+	}
+}
+
+void CIOHandler::removeModule(int id)
+{
+	for (int i = 0; i < modules.size(); i++)
+	{
+		if (modules.at(i)->getId() == id)
+		{
+			modules.erase(modules.begin() + i);
+			break;
+		}
+	}
+}
+
+void CIOHandler::saveJSON(std::string path)
+{
+	CJSONHandler json;
+	json.save(path, modules);
+}
+
+void CIOHandler::loadJSON(std::string path)
+{
+	CJSONHandler json;
+	json.load(path, modules);
+}
+
+void CIOHandler::stopModule(int id)
+{
+	for (IIOModule* m : modules)
+	{
+		if (m->getId() == id)
+		{
+			m->stop();
+			break;
+		}
+	}
+}
+
+void CIOHandler::exitApp()
+{
+	for (IIOModule* m : modules)
+	{
+		m->stop();
+	}
+	running = false;
+}
+
 
 CIOHandler::~CIOHandler()
 {
@@ -55,48 +150,32 @@ CIOHandler::~CIOHandler()
 
 void CIOHandler::callFunction(std::vector<std::string>* input)
 {
-	if (input->at(0) == "open")
+	if (input->at(0) == sOpen)
 	{
 		if (input->at(1) == "clientsocket")
 		{
-			std::string ip = input->at(2);
-			unsigned int port = atoi(input->at(3).c_str());
-			createClientSocket(ip, port);
+			createClientSocket(input->at(2), atoi(input->at(3).c_str()));
 		}
 		if (input->at(1) == "serversocket")
 		{
-			unsigned int port = atoi(input->at(3).c_str());
-			createServerSocket(port);
+			createServerSocket(atoi(input->at(2).c_str()));
 		}
 		else if (input->at(1) == "serial")
 		{
-			std::string port = input->at(2);
-			int baudrate = atoi(input->at(3).c_str());
-			createSerial(port, baudrate);
+			createSerial(input->at(2), atoi(input->at(3).c_str()));
 		}
 		else if (input->at(1) == "file")
 		{
-			std::string path = input->at(2);
-			createFile(path);
+			createFile(input->at(2));
 		}
 	}
-	else if (input->at(0) == "show")
+	else if (input->at(0) == sShow)
 	{
-		for (IIOModule* m : modules)
-		{
-			m->printInfo();
-		}
+		showModules();
 	}
-	else if (input->at(0) == "init")
+	else if (input->at(0) == sInit)
 	{
-		for (IIOModule* m : modules)
-		{
-			if (m->getId() == atoi(input->at(1).c_str()))
-			{
-				m->init();
-				break;
-			}
-		}
+		initModules(atoi(input->at(1).c_str()));
 	}
 	else if (input->at(0) == "output")
 	{
@@ -109,60 +188,29 @@ void CIOHandler::callFunction(std::vector<std::string>* input)
 			}
 		}
 	}
-	else if (input->at(0) == "connect")
+	else if (input->at(0) == sConnect)
 	{
-		IIOModule* first{};
-		for (IIOModule* m : modules)
-		{
-			if (m->getId() == atoi(input->at(1).c_str()))
-			{
-				first = m;
-			}
-		}
-		for (IIOModule* m : modules)
-		{
-			if (m->getId() == atoi(input->at(2).c_str()) && first != nullptr)
-			{
-				first->listenerTable.push_back(m);
-				first->connect();
-			}
-		}
+		connectModules(atoi(input->at(1).c_str()), atoi(input->at(2).c_str()));
 	}
-	else if (input->at(0) == "remove")
+	else if (input->at(0) == sRemove)
 	{
-		for (int i = 0; i < modules.size(); i++)
-		{
-			if (modules.at(i)->getId() == atoi(input->at(1).c_str()))
-			{
-				modules.erase(modules.begin()+i);
-				break;
-			}
-		}
+		removeModule(atoi(input->at(1).c_str()));
 	}
-	else if (input->at(0) == "save")
+	else if (input->at(0) == sSave)
 	{
-		CJSONHandler json;
-		json.save(input->at(1), modules);
+		saveJSON(input->at(1));
 	}
-	else if (input->at(0) == "load")
+	else if (input->at(0) == sLoad)
 	{
-		CJSONHandler json;
-		json.load(input->at(1), modules);
+		loadJSON(input->at(1));
 	}
-	else if (input->at(0) == "stop")
+	else if (input->at(0) == sStop)
 	{
-		for (IIOModule* m : modules)
-		{
-			m->stop();
-		}
+		stopModule(atoi(input->at(1).c_str()));
 	}
-	else if (input->at(0) == "exit")
+	else if (input->at(0) == sExit)
 	{
-		for (IIOModule* m : modules)
-		{
-			m->stop();
-		}
-		running = false;
+		exitApp();
 	}
 }
 
--- MultiIOHandler_DA/CIOHandler.h
+++ MultiIOHandler_DA/CIOHandler.h
@@ -18,7 +18,14 @@ private:
 	void createServerSocket(int unsigned port);
 	void createSerial(std::string port, int bauderate);
 	void createFile(std::string path);
-	void connect(int id1, int id2);
-	void disconnect();
+	void connectModules(int id1, int id2);
+	void showModules();
+	void initModules(int id);
+	void removeModule(int id);
+	void saveJSON(std::string path);
+	void loadJSON(std::string path);
+	void stopModule(int id);
+	void exitApp();
+
 
 };
